48dcd9fa6c7cf4279f59da6812b503945a59a465
LucasADiaz <lucasagdiaz@gmail.com>
2020-07-15 21:07:32 -0300

    docs agregado

---
+++ docs/precache-manifest.7df4cc4568ce42a49f853c423746eee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42cad6f543f0295572abb1e468873da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6798fcf85a49f1b6e8ee",
+    "url": "/static/css/main.6e6cfdd2.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6798fcf85a49f1b6e8ee",
+    "url": "/static/js/main.441a3ea5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
